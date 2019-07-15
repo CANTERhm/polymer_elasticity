d9fa1d62554d66263a7a7442354fec898c330a61
julianstar88 <jblaser@hm.edu>
2019-07-15 11:53:56 +0200

    finished: release v_2.2.0
    - updated the help-files (browser and .docx)
    - added a GNU GPL licence to the program

--- Callbacks.m
+++ Callbacks.m
@@ -1,5 +1,21 @@
 classdef Callbacks
     %CALLBACKS summary of all used callbacks in polymer_elasticity
+    %
+    % Copryright 2019 Julian Blaser
+    % This file is part of polymer_elasticity.
+    % 
+    % polymer_elasticity is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation, either version 3 of the License, or
+    % (at your option) any later version.
+    % 
+    % polymer_elasticity is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    % GNU General Public License for more details.
+    % 
+    % You should have received a copy of the GNU General Public License
+    % along with polymer_elasticity.  If not, see <http://www.gnu.org/licenses/>.
     
     methods(Static) % Button Callbacks
         
